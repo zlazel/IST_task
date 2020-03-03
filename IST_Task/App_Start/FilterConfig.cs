7174312e51c9ecd8dab1f51d8f894d7d965ec35a
ahmed.abdelrady <ahmed.g.abdelrady@gmail.com>
2020-03-03 22:54:24 +0200

    Add project files.

---
+++ IST_Task/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace IST_Task
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
