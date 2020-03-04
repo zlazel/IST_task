2be3454bf5f2377d3ad79fa89b43b393638d0aad
ahmed.abdelrady <ahmed.g.abdelrady@gmail.com>
2020-03-04 04:02:07 +0200

    edit school validations

--- IST_Task/Validation/SchoolVMValidator.cs
+++ IST_Task/Validation/SchoolVMValidator.cs
@@ -12,7 +12,9 @@ namespace IST_Task.Validation
             // FullName Validations
             RuleFor(x => x.Name).NotEmpty().WithMessage("*Required")
                 .MinimumLength(3).WithMessage("Minimum Length Is 3 Characters")
-                .MaximumLength(30).WithMessage("Minimum Length Is 30 Characters");
+                .MaximumLength(30).WithMessage("Minimum Length Is 30 Characters")
+                .Matches(@"^[\u0600-\u065F\u066A-\u06EF\u06FA-\u06FFa-zA-Z]+[\s\u0600-\u065F\u066A-\u06EF\u06FA-\u06FFa-zA-Z]*")
+                .WithMessage("Only Arabic and English Letters Allowed");
         }
     }
 }  
